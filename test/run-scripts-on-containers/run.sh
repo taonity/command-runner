166996d0b5c1cbb488dec5fdf7d3e2888283a830
Diulgher Artiom <artiom.diulgher@gmail.com>
2023-09-09 12:25:12 +0300

    Revert "d"
    This reverts commit 40a8ada2661b0b28fa6c65d3e67e2e634cc227e7.

--- test/run-scripts-on-containers/run.sh
+++ test/run-scripts-on-containers/run.sh
@@ -4,6 +4,11 @@ set -e
 
 cd "$(dirname $0)"
 
+. ../util.sh
+
+docker compose up -d --quiet-pull
+
+runner_exec_logs=$(docker compose exec command-runner runner World)
 
 if [[ $runner_exec_logs != *"Hello World !"* ]]; then
   fail "Failed to run before-all."
